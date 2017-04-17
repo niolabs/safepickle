cb749666c21bd7b8ffb794b781aca64d5c94e58b
Matt Dodge <mattedgod@gmail.com>
2017-04-17 10:25:04 -0700

    Set version to 0.1.0rc1

--- safepickle/__init__.py
+++ safepickle/__init__.py
@@ -2,7 +2,7 @@ from .safepickle import load, loads, dump, dumps
 
 
 __title__ = 'safepickle'
-__version__ = '1.0.0'
+__version__ = '0.1.0rc1'
 __author__ = 'n.io'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 n.io'
