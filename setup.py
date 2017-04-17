56c25985ab9e17c61c7b2017af20c44ccdc268c1
mattdodge <matt@n.io>
2017-04-17 10:47:41 -0700

    Add test command to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
+import sys
 import re
 from setuptools import setup, find_packages
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 with open('safepickle/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
@@ -17,7 +39,8 @@ setup(
         exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
 
     install_requires=[],
-    tests_require=[],
+    tests_require=['pytest', 'pytest-cov'],
+    cmdclass={'test': PyTest},
 
     author='n.io',
     author_email='info@n.io',
