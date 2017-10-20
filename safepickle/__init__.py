3956a04af4649aebf04d5416d35a8b1abd0b87c6
neutralioJenkins <jenkins@n.io>
2017-10-20 15:18:43 -0600

    Bump version to 0.1.0

--- safepickle/__init__.py
+++ safepickle/__init__.py
@@ -3,7 +3,7 @@ from .safepickle import PicklingError, UnpicklingError
 
 
 __title__ = 'safepickle'
-__version__ = '0.1.0rc4'
+__version__ = '0.1.0'
 __author__ = 'n.io'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 n.io Innovation, LLC'
